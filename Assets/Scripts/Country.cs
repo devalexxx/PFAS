9dc9445971418ed9ee4e978648a4d47f52596b8d
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-03-08 17:49:37 +0100

    feat: add Country component to country prefab

--- Assets/Scripts/Country.cs
+++ Assets/Scripts/Country.cs
@@ -6,9 +6,6 @@ namespace PFAS
     public class Country : MonoBehaviour
     {
         [field: SerializeField]
-        public string name { get; private set; }
-
-        [field: SerializeField]
         public List<City> cities { get; private set; }
 
         private void Awake()
