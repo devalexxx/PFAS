98965204c18e08d44e06a45cd6e2f78a7216871b
iTschoCapic <52358452+iTschoCapic@users.noreply.github.com>
2025-03-08 15:10:55 +0100

    Feature : Skilltrees (Not Visual)

---
+++ Assets/Scripts/Skills Tree/Skill.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    public string name { get; private set; }
+    public string description { get; private set; }
+    public int cost { get; private set; }
+    public int regulation { get; private set; }
+    public int technologie { get; private set; }
+    public int prevention { get; private set; }
+    public int globalPollution { get; private set; }
+
+
+    public Skill(string p_name, string p_description, int p_cost, int p_regulation, int p_technologie, int p_prevention, int p_globalPollution)
+    {
+        name = p_name;
+        description = p_description;
+        cost = p_cost;
+        regulation = p_regulation;
+        technologie = p_technologie;
+        prevention = p_prevention;
+        globalPollution = p_globalPollution;
+    }
+}
