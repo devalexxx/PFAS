bf0972a82e80c561abc1e7ccfa6b582222850da4
devalexxx <46360108+devalexxx@users.noreply.github.com>
2025-03-08 15:20:56 +0100

    chore: namespace

--- Assets/Scripts/Skills Tree/IntelligenceTree.cs
+++ Assets/Scripts/Skills Tree/IntelligenceTree.cs
@@ -1,31 +1,32 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using UnityEngine.InputSystem;
-using UnityEngine.SocialPlatforms;
 
-public class IntelligenceTree : MonoBehaviour
+namespace PFAS.Gameplay
 {
-    private Skill[] _globalNetworkOfEnvironmentalSensors;
-    private Skill[] _targetedAwarenessCampaigns;
-    private Skill[] _industrialTracing;
-    private Skill[] _scenarioSimulation;
-    void Start()
+    public class IntelligenceTree : MonoBehaviour
     {
-        _globalNetworkOfEnvironmentalSensors[0] = new Skill("Satellite deployment", "", 1, 0, 5, 10, -6);
-        _globalNetworkOfEnvironmentalSensors[1] = new Skill("Autonomous terrestrial sensors", "", 2, 0, 4, 12, -7);
-        _globalNetworkOfEnvironmentalSensors[2] = new Skill("Artificial intelligence for analysis", "", 3, 0, 6, 15, -8);
+        private Skill[] _globalNetworkOfEnvironmentalSensors;
+        private Skill[] _targetedAwarenessCampaigns;
+        private Skill[] _industrialTracing;
+        private Skill[] _scenarioSimulation;
+        void Start()
+        {
+            _globalNetworkOfEnvironmentalSensors[0] = new Skill("Satellite deployment", "", 1, 0, 5, 10, -6);
+            _globalNetworkOfEnvironmentalSensors[1] = new Skill("Autonomous terrestrial sensors", "", 2, 0, 4, 12, -7);
+            _globalNetworkOfEnvironmentalSensors[2] = new Skill("Artificial intelligence for analysis", "", 3, 0, 6, 15, -8);
 
-        _targetedAwarenessCampaigns[0] = new Skill("Public education", "", 5, 5, 0, 8, -4);
-        _targetedAwarenessCampaigns[1] = new Skill("Industrial training", "", 6, 6, 0, 10, -5);
-        _targetedAwarenessCampaigns[2] = new Skill("Citizen mobilisation", "", 7, 7, 0, 12, -6);
+            _targetedAwarenessCampaigns[0] = new Skill("Public education", "", 5, 5, 0, 8, -4);
+            _targetedAwarenessCampaigns[1] = new Skill("Industrial training", "", 6, 6, 0, 10, -5);
+            _targetedAwarenessCampaigns[2] = new Skill("Citizen mobilisation", "", 7, 7, 0, 12, -6);
 
-        _industrialTracing[0] = new Skill("Production monitoring system", "", 8, 8, 0, 10, -6);
-        _industrialTracing[1] = new Skill("Reinforced audits", "", 5, 5, 0, 12, -7);
-        _industrialTracing[2] = new Skill("Penalties for concealment", "", 8, 8, 0, 6, -5);
+            _industrialTracing[0] = new Skill("Production monitoring system", "", 8, 8, 0, 10, -6);
+            _industrialTracing[1] = new Skill("Reinforced audits", "", 5, 5, 0, 12, -7);
+            _industrialTracing[2] = new Skill("Penalties for concealment", "", 8, 8, 0, 6, -5);
 
-        _scenarioSimulation[0] = new Skill("Predictive contamination models", "", 1, 0, 5, 10, -6);
-        _scenarioSimulation[1] = new Skill("Impact studies on flora and fauna", "", 2, 0, 4, 8, -4);
-        _scenarioSimulation[2] = new Skill("Preparing for health crises", "", 3, 5, 0, 12, -7);
+            _scenarioSimulation[0] = new Skill("Predictive contamination models", "", 1, 0, 5, 10, -6);
+            _scenarioSimulation[1] = new Skill("Impact studies on flora and fauna", "", 2, 0, 4, 8, -4);
+            _scenarioSimulation[2] = new Skill("Preparing for health crises", "", 3, 5, 0, 12, -7);
+        }
+
     }
-
 }
+
--- Assets/Scripts/Skills Tree/PoliticalTree.cs
+++ Assets/Scripts/Skills Tree/PoliticalTree.cs
@@ -1,30 +1,31 @@
 using UnityEngine;
-using UnityEngine.SocialPlatforms;
-using UnityEngine.UIElements;
 
-public class PoliticalTree : pfas.gameplay.GlobalTree
+namespace PFAS.Gameplay
 {
-    private Skill[] _acceleratedLegislation;
-    private Skill[] _environmentalSubsidies;
-    private Skill[] _internationalStandards;
-    private Skill[] _pfasTaxes;
-
-    private void Start()
+    public class PoliticalTree : GlobalTree
     {
-        _acceleratedLegislation[0] = new Skill("Regulatory emergency commitees", "", 1, 8, 0, 0, -5);
-        _acceleratedLegislation[1] = new Skill("Simplified procedures", "", 2, 10, 0, 0, -7);
-        _acceleratedLegislation[2] = new Skill("Progressive bans", "", 3, 12, 0, 0, -10);
+        private Skill[] _acceleratedLegislation;
+        private Skill[] _environmentalSubsidies;
+        private Skill[] _internationalStandards;
+        private Skill[] _pfasTaxes;
 
-        _environmentalSubsidies[0] = new Skill("Tax incentives", "", 5, 5, 0, 0, -5);
-        _environmentalSubsidies[1] = new Skill("Research grants", "", 8, 4, 8, 0, 0);
-        _environmentalSubsidies[2] = new Skill("Eco-label", "", 3, 0, 0, 6, -3);
+        private void Start()
+        {
+            _acceleratedLegislation[0] = new Skill("Regulatory emergency commitees", "", 1, 8, 0, 0, -5);
+            _acceleratedLegislation[1] = new Skill("Simplified procedures", "", 2, 10, 0, 0, -7);
+            _acceleratedLegislation[2] = new Skill("Progressive bans", "", 3, 12, 0, 0, -10);
 
-        _internationalStandards[0] = new Skill("Global treaties", "", 4, 12, 0, 0, -8);
-        _internationalStandards[1] = new Skill("Harmonisation of thresholds", "", 5, 10, 0, 5, -6);
-        _internationalStandards[2] = new Skill("Intergovernmental cooperation", "", 6, 8, 0, 4, -5);
+            _environmentalSubsidies[0] = new Skill("Tax incentives", "", 5, 5, 0, 0, -5);
+            _environmentalSubsidies[1] = new Skill("Research grants", "", 8, 4, 8, 0, 0);
+            _environmentalSubsidies[2] = new Skill("Eco-label", "", 3, 0, 0, 6, -3);
 
-        _pfasTaxes[0] = new Skill("Progressive taxation", "", 7, 10, 0, 0, -7);
-        _pfasTaxes[1] = new Skill("Fines for pollution", "", 8, 6, 0, 0, -5);
-        _pfasTaxes[2] = new Skill("Reinvestment of taxes", "", 9, 0, 8, 0, -6);
+            _internationalStandards[0] = new Skill("Global treaties", "", 4, 12, 0, 0, -8);
+            _internationalStandards[1] = new Skill("Harmonisation of thresholds", "", 5, 10, 0, 5, -6);
+            _internationalStandards[2] = new Skill("Intergovernmental cooperation", "", 6, 8, 0, 4, -5);
+
+            _pfasTaxes[0] = new Skill("Progressive taxation", "", 7, 10, 0, 0, -7);
+            _pfasTaxes[1] = new Skill("Fines for pollution", "", 8, 6, 0, 0, -5);
+            _pfasTaxes[2] = new Skill("Reinvestment of taxes", "", 9, 0, 8, 0, -6);
+        }
     }
 }
