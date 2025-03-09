999854829f1911d7f4e3304849036de8839c4132
iTschoCapic <52358452+iTschoCapic@users.noreply.github.com>
2025-03-09 10:57:03 +0100

    Updated : SkillTree

--- Assets/Scripts/Skills Tree/Skill.cs
+++ Assets/Scripts/Skills Tree/Skill.cs
@@ -7,13 +7,15 @@ namespace PFAS.Gameplay
         public string name { get; private set; }
         public string description { get; private set; }
         public int cost { get; private set; }
-        public int regulation { get; private set; }
-        public int technologie { get; private set; }
-        public int prevention { get; private set; }
-        public int globalPollution { get; private set; }
-
-
-        public Skill(string p_name, string p_description, int p_cost, int p_regulation, int p_technologie, int p_prevention, int p_globalPollution)
+        public float regulation { get; private set; }
+        public float technologie { get; private set; }
+        public float prevention { get; private set; }
+        public float globalPollution { get; private set; }
+        public Skill[] after { get; private set; }
+        public Skill[] previous { get; private set; }
+        public bool unlocked { get; private set; }
+        public bool purchased { get; private set; } = false;
+        public Skill(string p_name, string p_description, int p_cost, float p_regulation, float p_technologie, float p_prevention, float p_globalPollution, bool p_unlocked, Skill[] p_after = default(Skill[]), Skill[] p_previous = default(Skill[]))
         {
             name = p_name;
             description = p_description;
@@ -22,6 +24,42 @@ namespace PFAS.Gameplay
             technologie = p_technologie;
             prevention = p_prevention;
             globalPollution = p_globalPollution;
+            unlocked = p_unlocked;
+            after = p_after;
+            previous = p_previous;
+        }
+
+        public void BuySkill(int competencePoints)
+        {
+            if (competencePoints >= cost)
+            {
+                if (unlocked)
+                {
+                    GameManager.instance.UpdateStats(regulation, technologie, prevention, globalPollution, cost);
+                    purchased = true;
+                    foreach (Skill skill in after)
+                    {
+                        skill.CheckUnlockSkill();
+                    }
+                }
+            }
+        }
+        public void CheckUnlockSkill()
+        {
+            if (previous.Length == 0)
+            {
+                unlocked = true;
+            }
+            else
+            {
+                foreach (Skill skill in previous)
+                {
+                    if (skill.purchased)
+                    {
+                        unlocked = true;
+                    }
+                }
+            }
         }
     }
 }
