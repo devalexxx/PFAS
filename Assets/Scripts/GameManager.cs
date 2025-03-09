669fd73022b763f80c70b91b4f9dffb506db0e5d
iTschoCapic <52358452+iTschoCapic@users.noreply.github.com>
2025-03-09 10:57:03 +0100

    Updated : SkillTree

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,13 @@ namespace PFAS
     {
         public static GameManager instance;
 
+        public float regulation = 0;
+        public float technologie = 0;
+        public float prevention = 0;
+        public float globalPollution = 0;
+
+        public int competencePoints = 0;
+
         public List<City> cities;
 
         private void Awake()
@@ -14,6 +21,14 @@ namespace PFAS
             instance = this;
             cities.ForEach(city => city.Setup());
         }
+
+        public void UpdateStats(float p_regulation, float p_technologie, float p_prevention, float p_globalPollution, int cost)
+        {
+            regulation += p_regulation;
+            technologie += p_technologie;
+            prevention += p_prevention;
+            globalPollution += p_globalPollution;
+            competencePoints -= cost;
+        }
     }
 }
-
--- Assets/Scripts/Skills Tree/PoliticalTree.cs
+++ Assets/Scripts/Skills Tree/PoliticalTree.cs
@@ -11,21 +11,21 @@ namespace PFAS.Gameplay
 
         private void Start()
         {
-            _acceleratedLegislation[0] = new Skill("Regulatory emergency commitees", "", 1, 8, 0, 0, -5);
-            _acceleratedLegislation[1] = new Skill("Simplified procedures", "", 2, 10, 0, 0, -7);
-            _acceleratedLegislation[2] = new Skill("Progressive bans", "", 3, 12, 0, 0, -10);
+            _acceleratedLegislation[0] = new Skill("Regulatory emergency commitees", "", 1, 8, 0, 0, -5, false);
+            _acceleratedLegislation[1] = new Skill("Simplified procedures", "", 2, 10, 0, 0, -7, false);
+            _acceleratedLegislation[2] = new Skill("Progressive bans", "", 3, 12, 0, 0, -10, false);
 
-            _environmentalSubsidies[0] = new Skill("Tax incentives", "", 5, 5, 0, 0, -5);
-            _environmentalSubsidies[1] = new Skill("Research grants", "", 8, 4, 8, 0, 0);
-            _environmentalSubsidies[2] = new Skill("Eco-label", "", 3, 0, 0, 6, -3);
+            _environmentalSubsidies[0] = new Skill("Tax incentives", "", 5, 5, 0, 0, -5, false);
+            _environmentalSubsidies[1] = new Skill("Research grants", "", 8, 4, 8, 0, 0, false);
+            _environmentalSubsidies[2] = new Skill("Eco-label", "", 3, 0, 0, 6, -3, false);
 
-            _internationalStandards[0] = new Skill("Global treaties", "", 4, 12, 0, 0, -8);
-            _internationalStandards[1] = new Skill("Harmonisation of thresholds", "", 5, 10, 0, 5, -6);
-            _internationalStandards[2] = new Skill("Intergovernmental cooperation", "", 6, 8, 0, 4, -5);
+            _internationalStandards[0] = new Skill("Global treaties", "", 4, 12, 0, 0, -8, false);
+            _internationalStandards[1] = new Skill("Harmonisation of thresholds", "", 5, 10, 0, 5, -6, false);
+            _internationalStandards[2] = new Skill("Intergovernmental cooperation", "", 6, 8, 0, 4, -5, false);
 
-            _pfasTaxes[0] = new Skill("Progressive taxation", "", 7, 10, 0, 0, -7);
-            _pfasTaxes[1] = new Skill("Fines for pollution", "", 8, 6, 0, 0, -5);
-            _pfasTaxes[2] = new Skill("Reinvestment of taxes", "", 9, 0, 8, 0, -6);
+            _pfasTaxes[0] = new Skill("Progressive taxation", "", 7, 10, 0, 0, -7, false);
+            _pfasTaxes[1] = new Skill("Fines for pollution", "", 8, 6, 0, 0, -5, false);
+            _pfasTaxes[2] = new Skill("Reinvestment of taxes", "", 9, 0, 8, 0, -6, false);
         }
     }
 }
