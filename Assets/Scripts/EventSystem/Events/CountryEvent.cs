49ec4aa617c70a42123243925a51b7f33acfcecf
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-09 14:23:42 +0100

    feat: add city event

--- Assets/Scripts/EventSystem/Events/CountryEvent.cs
+++ Assets/Scripts/EventSystem/Events/CountryEvent.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyBox;
+using PFAS.Stats;
+using PFAS.Utils;
 using UnityEngine;
 
 
@@ -5,14 +11,42 @@ namespace PFAS.EventSystem.Events
 {
     public class CountryEvent : IEvent
     {
+        [Separator]
+        [Header("Country")]
+        public EnumArray<CityStats, float> statsToChange = new EnumArray<CityStats, float>();
+        public bool asCountryCondition = false;
+
+        Country _country;
+
         public bool CanUse()
         {
-            throw new System.NotImplementedException();
+            return true;
+        }
+
+        public Country GetCountry()
+        {
+            List<Country> t_countries = GameManager.instance.countries;
+            if (asCountryCondition)
+            {
+                return null;
+            }
+            else
+            {
+                return t_countries[UnityEngine.Random.Range(0, t_countries.Count)];
+            }
         }
 
         public void Use()
         {
-            throw new System.NotImplementedException();
+            _country = GetCountry();
+            if (_country == null) return;
+
+            Enum.GetValues(typeof(CityStats))
+                .Cast<CityStats>()
+                .Where(stat => statsToChange[stat] != 0)
+                .ToList()
+                .ForEach(stat => _country.OnEvent(stat, statsToChange[stat]));
         }
+        
     }
 }
