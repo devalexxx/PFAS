efb94050269a250d02e398df9b7f8761b6593c57
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-09 15:11:17 +0100

    add comentaire

--- Assets/Scripts/EventSystem/EventManager.cs
+++ Assets/Scripts/EventSystem/EventManager.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using PFAS.EventSystem.Events;
 using PFAS.Objects;
-using PFAS.Stats;
 using TMPro;
 using UnityEngine;
 
@@ -11,11 +8,26 @@ namespace PFAS.EventSystem
 {
     public class EventManager : MonoBehaviour
     {
+        /// <summary>
+        /// The UI panel that displays event information.
+        /// </summary>
         [Header("UI")]
         public GameObject eventPanel;
+
+        /// <summary>
+        /// The UI Text element that displays the title of the event.
+        /// </summary>
         public TextMeshProUGUI eventTitle;
+
+        /// <summary>
+        /// The UI Text element that displays the description of the event.
+        /// </summary>
         public TextMeshProUGUI eventDescription;
 
+
+        /// <summary>
+        /// A list of available events in the game.
+        /// </summary>
         List<EventObject> _events = new List<EventObject>();
 
         private void Start()
@@ -33,6 +45,10 @@ namespace PFAS.EventSystem
             }
         }
 
+        /// <summary>
+        /// This function selects a random event from the list of available events that can be used.
+        /// </summary>
+        /// <returns>Returns a randomly selected EventObject that is available for use.</returns>
         public EventObject GetRandomEvent()
         {
             List<EventObject> t_events = new List<EventObject>();
@@ -41,6 +57,10 @@ namespace PFAS.EventSystem
             return t_events[UnityEngine.Random.Range(0, t_events.Count)];
         }
 
+        /// <summary>
+        /// This function displays the event panel and sets the event title and description in the UI. 
+        /// It also invokes the event to apply any changes or actions associated with it.
+        /// </summary>
         public void ShowEvent()
         {
             var t_event = GetRandomEvent();
--- Assets/Scripts/EventSystem/Events/CityEvent.cs
+++ Assets/Scripts/EventSystem/Events/CityEvent.cs
@@ -10,22 +10,52 @@ namespace PFAS.EventSystem.Events
 {
     public class CityEvent : IEvent
     {
+        /// <summary>
+        /// A variable that holds an EnumArray mapping CityStats to corresponding float values. It is used to track and modify statistics for cities.
+        /// </summary>
         [Separator]
         [Header("City")]
         public EnumArray<CityStats, float> statsToChange = new EnumArray<CityStats, float>();
+
+        /// <summary>
+        /// A flag indicating whether the current logic is based on city conditions.
+        /// </summary>
         public bool asCityCondition = false;
+
+        /// <summary>
+        /// A flag indicating whether the event should only apply to the capital city.
+        /// </summary>
         [ConditionalField(nameof(asCityCondition))] public bool onlyOnCapital = false;
+
+        /// <summary>
+        /// A list of maximum values for different statistics that a city must have to meet the conditions.
+        /// </summary>
         [ConditionalField(nameof(asCityCondition))] public EnumArray<CityStats, float> maxStats = new EnumArray<CityStats, float>();
+
+        /// <summary>
+        /// A list of minimum values for different statistics that a city must have to meet the conditions.
+        /// </summary>
         [ConditionalField(nameof(asCityCondition))] public EnumArray<CityStats, float> minStats = new EnumArray<CityStats, float>();
 
-
+        /// <summary>
+        /// A private field representing the city that is currently selected or being used.
+        /// </summary>
         City _city;
 
+
+        /// <summary>
+        /// This function checks if the event can be performed.
+        /// </summary>
+        /// <returns>Returns a boolean value indicating if the event can be performed/returns>
         public bool CanUse()
         {
             return true;
         }
 
+        /// <summary>
+        /// This function returns a string representation of the actions taken on the city, including the statistics that were changed and their values.
+        /// </summary>
+        /// <returns>Returns a string describing the changes made to the city's statistics.</returns>
         public override string ToString()
         {
             var changes = new List<string>();
@@ -45,6 +75,11 @@ namespace PFAS.EventSystem.Events
                 : $"La ville {_city.name} ne gagne rien.";
         }
 
+
+        /// <summary>
+        /// This function selects a random city based on the given conditions. If asCityCondition is true, it filters cities based on the city condition.
+        /// </summary>
+        /// <returns>Returns a random City object that matches the given conditions, or null if no city matches.</returns>
         public City GetRandomCity()
         {
             List<City> t_cities = GameManager.instance.GetAllCities();
@@ -60,6 +95,12 @@ namespace PFAS.EventSystem.Events
                 : null;
         }
 
+
+        /// <summary>
+        /// This function checks if the selected city matches the statistics conditions (max and min values) defined for the event.
+        /// </summary>
+        /// <param name="city">The city to be checked against the conditions.</param>
+        /// <returns>Returns true if the city matches the statistics conditions, otherwise false.</returns>
         private bool MatchesStats(City city)
         {
             int correctValues = 0, maxValues = 0;
@@ -81,6 +122,10 @@ namespace PFAS.EventSystem.Events
             return correctValues >= maxValues;
         }
 
+
+        /// <summary>
+        /// This function applies the selected statistics changes to the chosen city.
+        /// </summary>
         public void Use()
         {
             _city = GetRandomCity();
