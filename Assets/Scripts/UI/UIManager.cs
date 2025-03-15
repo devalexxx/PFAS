1cb7e4f0de7316ff43c196537beb759dafa488ec
Reyane <38364033+RReyane@users.noreply.github.com>
2025-03-15 15:12:37 +0100

    style: added namespace

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,23 +1,39 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class UIManager : MonoBehaviour
-{
-    [SerializeField] private InputActionReference _pauseInput;
-    [SerializeField] private UIPausePanel         _pausePanel;
-
-    void Awake()
+namespace PFAS.UI {
+    public class UIManager : MonoBehaviour
     {
-        _pauseInput.action.performed += _ => _pausePanel.Toggle();
-    }
+        [Header("Pause Panel")]
+        [SerializeField] private InputActionReference _pauseInput;
+        [SerializeField] private UIPausePanel         _pausePanel;
 
-    void OnEnable()
-    {
-        _pauseInput.action.Enable();
-    }
+        [Header("Country Panel")]
+        [SerializeField] private UICountryPanel _countryPanel;
 
-    void OnDisable()
-    {
-        _pauseInput.action.Disable();
+        void Awake()
+        {
+            _pauseInput.action.performed += _ => _pausePanel.Toggle();
+        }
+
+        void OnEnable()
+        {
+            _pauseInput.action.Enable();
+        }
+
+        void OnDisable()
+        {
+            _pauseInput.action.Disable();
+        }
+
+        public void ShowCountryPanel(GameObject p_selectedCountry)
+        {
+            _countryPanel.Show(p_selectedCountry);
+        }
+
+        public void HideCountryPanel()
+        {
+            _countryPanel.Hide();
+        }
     }
 }
