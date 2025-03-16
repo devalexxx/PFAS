9f091a342e94c3df0cbb2e2751d4b31af3518257
Reyane <38364033+RReyane@users.noreply.github.com>
2025-03-16 23:25:36 +0100

    fix: smoothly transition camera position and zoom to align selected country at target viewport position

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -26,10 +27,6 @@ namespace PFAS.Cam
         [SerializeField] private Vector2 _panLimitMax;
         private bool _canEdgePan = true;
 
-        [Header("Country Relocation Position")]
-        //coordinates of the relocation position
-        [SerializeField] private Vector2 targetViewportPos = new(0.75f, 0.25f);
-
         private Camera _cam;
 
         [Header("Input Action References")]
@@ -39,6 +36,13 @@ namespace PFAS.Cam
 
         private bool _isDragging = false;
         private Vector2 _lastDragScreenPos;
+
+        [Header("Smooth Camera Translation")]
+        //coordinates of the relocation position
+        [SerializeField] private Vector2 targetViewportPos = new(0.75f, 0.25f);
+        [SerializeField] private float _targetZoomToCountry = 10f;
+        [SerializeField] private float _cameraTransitionDuration = 0.5f;
+        private Coroutine _cameraTransitionCoroutine;
 
         private void Awake()
         {
@@ -178,15 +182,35 @@ namespace PFAS.Cam
         // Move the camera so that the country is at the relocation position
         public void RelocateCountry(Vector3 p_countryPos)
         {
-            float t_distance = -_cam.transform.position.z;
-            // Relocation Position in world space
-            Vector3 t_relocationWorldPos = _cam.ViewportToWorldPoint(new(targetViewportPos.x, targetViewportPos.y,t_distance));
-            // We want t_relocationWorldPos to become p_countryPos, so delta is :
-            Vector3 t_delta = p_countryPos - t_relocationWorldPos;
-            Vector3 t_targetCamPos = transform.position + t_delta;
-            t_targetCamPos = _ClampCameraPosition(t_targetCamPos);
-            transform.position = t_targetCamPos;
+            Vector3 t_offset = new((targetViewportPos.x - 0.5f) * 2f * _targetZoomToCountry * _cam.aspect,
+                                    (targetViewportPos.y - 0.5f) * 2f * _targetZoomToCountry,
+                                    0f
+             );
 
+            Vector3 t_targetCamPos = _ClampCameraPosition(p_countryPos - t_offset);
+
+            // Smoothly move the camera to the new position
+            if (_cameraTransitionCoroutine != null)
+                StopCoroutine(_cameraTransitionCoroutine);
+
+            _cameraTransitionCoroutine = StartCoroutine(_SmoothMoveCamera(transform.position, t_targetCamPos, _cam.orthographicSize, _targetZoomToCountry, _cameraTransitionDuration));
+
+        }
+
+        private IEnumerator _SmoothMoveCamera(Vector3 p_startPos, Vector3 p_endPos, float p_startZoom, float p_endZoom, float p_duration)
+        {
+            float t_elapsed = 0;
+            while (t_elapsed < p_duration)
+            {
+                t_elapsed += Time.deltaTime;
+                transform.position = Vector3.Lerp(p_startPos, p_endPos, t_elapsed / p_duration);
+                float t_ratio = t_elapsed / p_duration;
+                transform.position = Vector3.Lerp(p_startPos, p_endPos, t_ratio);
+                _cam.orthographicSize = Mathf.Lerp(p_startZoom, p_endZoom, t_ratio);
+                yield return null;
+            }
+            transform.position = p_endPos;
+            _cam.orthographicSize = p_endZoom;
         }
 
         #endregion
