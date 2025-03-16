ba4d64f39dcb86f70b8f7b111cd025e284a473a2
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-16 15:59:03 +0100

    fix: fix rebase

--- Assets/Scripts/EventSystem/EventManager.cs
+++ Assets/Scripts/EventSystem/EventManager.cs
@@ -137,41 +137,5 @@ namespace PFAS.SystemEvent
         {
             return p_polygonCollider.OverlapPoint(point);
         }
-
-        public void SpawnObjectInside(PolygonCollider2D p_polygonCollider)
-        {
-            if (p_polygonCollider == null || moneyBuble == null) return;
-
-            int t_attempts = 0;
-            Vector2 t_spawnPos;
-
-            do
-            {
-                t_spawnPos = _GetRandomPointInBounds(p_polygonCollider.bounds);
-                t_attempts++;
-            }
-            while (!_IsPointInsidePolygon(t_spawnPos, p_polygonCollider) && t_attempts < maxAttempts);
-
-            if (t_attempts < maxAttempts)
-            {
-                Instantiate(moneyBuble, t_spawnPos, Quaternion.identity);
-            }
-            else
-            {
-                Debug.LogWarning("Impossible de trouver une position valide dans le polygone !");
-            }
-        }
-
-        Vector2 _GetRandomPointInBounds(Bounds bounds)
-        {
-            float x = Random.Range(bounds.min.x, bounds.max.x);
-            float y = Random.Range(bounds.min.y, bounds.max.y);
-            return new Vector2(x, y);
-        }
-
-        bool _IsPointInsidePolygon(Vector2 point, PolygonCollider2D p_polygonCollider)
-        {
-            return p_polygonCollider.OverlapPoint(point);
-        }
     }
 }
