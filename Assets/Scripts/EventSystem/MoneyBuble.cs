4d65dfa55947f9eb7533ace11ae8a44fff61cf5b
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-16 15:30:29 +0100

    feat: buble reduce

--- Assets/Scripts/EventSystem/MoneyBuble.cs
+++ Assets/Scripts/EventSystem/MoneyBuble.cs
@@ -1,22 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class MoneyBuble : MonoBehaviour, IPointerClickHandler
 {
+    public float lifeTime = 5;
+    public float reduceTime = 2;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("click");
+        Destroy(gameObject);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        StartCoroutine(Duration());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Duration()
     {
-        
+        yield return new WaitForSeconds(lifeTime);
+
+        float t_time = 0;
+        int t_steps = Mathf.CeilToInt(reduceTime / 0.1f); // Nombre d'itérations
+        Vector3 t_scaleStep = transform.localScale / t_steps; // Réduction exacte par step
+
+        while (t_time < reduceTime)
+        {
+            yield return new WaitForSeconds(0.1f);
+            t_time += 0.1f;
+            transform.localScale -= t_scaleStep;
+        }
+
+        // S'assurer que la taille est bien à zéro après la boucle
+        transform.localScale = Vector3.zero;
+        Destroy(gameObject);
     }
+
 }
