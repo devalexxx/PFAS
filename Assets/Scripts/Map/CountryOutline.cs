737fd60f1ce942a86a5a8521c2a3971647806da3
AnNaNnq <97672945+AnNaNnq@users.noreply.github.com>
2025-03-17 14:42:58 +0100

    refactor: name highlighted files

--- Assets/Scripts/Map/CountryOutline.cs
+++ Assets/Scripts/Map/CountryOutline.cs
@@ -14,7 +14,7 @@ namespace PFAS.Map
 
         public void Outline()
         {
-            string t_spritePath = $"Sprites/Map/{name}Highlighted";
+            string t_spritePath = $"Sprites/Map/{name}_Highlighted";
             _renderer.sprite = Resources.Load<Sprite>(t_spritePath);
 
             transform.position += new Vector3(0, 0, -2);
