da4280dc622340505119709b7aae40cba87ba72e
Xoletis <47059364+Xoletis@users.noreply.github.com>
2025-03-22 16:58:12 +0100

    feat: add campagne

---
+++ Assets/Scripts/Camapagnes/CampagnesManager.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using MyBox;
+using PFAS.Stats;
+using PFAS.Timer;
+using UnityEngine;
+
+namespace PFAS.Campagnes
+{
+    public class CampagnesManager : MonoBehaviour
+    {
+        [ReadOnly]
+        public List<Campagne> campagnes;
+
+        private void OnEnable()
+        {
+            TimerManager.OnTick += OnDay;
+        }
+
+        private void OnDisable()
+        {
+            TimerManager.OnTick -= OnDay;
+        }
+
+        public void OnDay()
+        {
+            campagnes.ForEach(c => c.timeBeforeFinish--);
+            campagnes.RemoveAll(c => c.timeBeforeFinish <= 0);
+        }
+
+    }
+
+    [System.Serializable]
+    public class Campagne
+    {
+        public CityStats stats;
+        public City city;
+        public int amount;
+        public int timeBeforeFinish;
+
+        public Campagne(CityStats t_stats, City t_city, int t_amount, int t_timeBeforeFinish)
+        {
+            stats = t_stats;
+            city = t_city;
+            amount = t_amount;
+            timeBeforeFinish = t_timeBeforeFinish;
+        }
+    }
+}
--- Assets/Scripts/Camapagnes/CampagnesUI.cs
+++ Assets/Scripts/Camapagnes/CampagnesUI.cs
@@ -10,17 +10,56 @@ namespace PFAS.Campagnes
     {
         public TMP_Dropdown campagneDropdown;
         public Slider campagneSlider;
+        public TextMeshProUGUI moneyText, timeText, amountText;
+
+        public CampagnesManager manager;
+
+        int _currentMoneyToUse;
+
+        City _city;
+        CityStats _cityStats;
 
         public void SetUpCampagne(City p_city)
         {
+            _cityStats = CityStats.Adaptability;
             campagneDropdown.ClearOptions(); 
 
             foreach(var stat in Enum.GetValues(typeof(CityStats)))
             {
-                TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData();
-                option.text = stat.ToString();
-                campagneDropdown.options.Add(option);
+                TMP_Dropdown.OptionData t_option = new TMP_Dropdown.OptionData();
+                t_option.text = stat.ToString();
+                campagneDropdown.options.Add(t_option);
             }
+
+            _city = p_city;
+
+            campagneSlider.minValue = 0;
+            campagneSlider.maxValue = GameManager.instance.money;
+            _currentMoneyToUse = 0;
+            campagneSlider.value = 0;
+            moneyText.text = "0";
+            timeText.text = "";
+            amountText.text = "0";
+        }
+
+        public void SetCurrentMoney(float p_value)
+        {
+            _currentMoneyToUse = (int)p_value;
+            moneyText.text = _currentMoneyToUse.ToString();
+            amountText.text = _currentMoneyToUse.ToString();
+            timeText.text = _currentMoneyToUse + " j";
+        }
+
+        public void SetStat(string p_stat)
+        {
+            _cityStats = (CityStats)Enum.Parse(typeof(CityStats), campagneDropdown.itemText.text);
+        }
+
+        public void AddCampagne()
+        {
+            
+            Campagne t_campagne = new Campagne(_cityStats, _city, _currentMoneyToUse, _currentMoneyToUse);
+            manager.campagnes.Add(t_campagne);
         }
     }
 }
