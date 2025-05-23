ea4fd76623765a2e10c1a802c5737a6add4d8fbd
RReyane <fridom42@outlook.fr>
2025-05-23 22:08:10 +0200

    refactor: changed time slider for buttons

--- Assets/Scripts/Timer/TimerManager.cs
+++ Assets/Scripts/Timer/TimerManager.cs
@@ -11,8 +11,8 @@ namespace PFAS.Timer
         [Header("UI")]
         // Assign the UI Text for the date
         [SerializeField] private TextMeshProUGUI _timerText;
-        // Assign the Slider for the time scale
-        [SerializeField] private Slider _timeScaleInput;
+        // Assign the UI Text for the time scale
+        [SerializeField] private TextMeshProUGUI _timeScaleText;
 
         [Header("Time Settings")]
         // Time scale multiplier: 1 = normal speed, 0 = pause, >1 = accelerated (can only use integer)
@@ -32,9 +32,10 @@ namespace PFAS.Timer
         // should subscribe to this event for other scripts to update at each tick
         public static event TickAction OnTick;
 
-        private void Awake()
+        private void Start()
         {
             currentDate = startDate;
+            SetTimeScale(0);
             _UpdateTimerUI();
         }
 
@@ -74,16 +75,24 @@ namespace PFAS.Timer
             }
         }
 
+        private void _UpdateTimeScaleUI()
+        {
+            if (_timeScaleText != null)
+            {
+                // Update the time scale text in the UI
+                _timeScaleText.text = 'x'+_timeScale.ToString();
+            }
+        }
+
         // Method to modify the time speed from other scripts
         public void SetTimeScale(float newTimeScale)
         {
             _timeScale = (int)newTimeScale;
 
-            // If _timeScale is modified by something else than the slider, we update it
-            if (_timeScaleInput != null && _timeScaleInput.value != newTimeScale)
-            {
-                _timeScaleInput.value = _timeScale;
-            }
+            // Update the time scale text in the UI
+            _UpdateTimeScaleUI();
         }
+
+
     }
 }
